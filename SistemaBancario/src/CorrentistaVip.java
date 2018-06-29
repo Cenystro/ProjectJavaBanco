7fbd724acf3d977338c0d5e4e19cef1e074bf0fd
Cenystro <Cenystro@192.168.15.3>
2018-06-28 21:16:37 -0300

    Faltou esse

--- SistemaBancario/src/CorrentistaVip.java
+++ SistemaBancario/src/CorrentistaVip.java
@@ -1,4 +1,42 @@
+//Subclasse
+public class CorrentistaVip extends Correntista {
+	private static int proxCodigo = 1;
+	private int codigo;
+	private String nome;
+	private String nomeResposavel;
+	
+	public CorrentistaVip (String nome, String nomeResponsavel){
+		this.codigo = getProxCodigo();
+		this.nome = nome;
+		
+	}
+	
+	public String getNomeResposavel() {
+		return nomeResposavel;
+	}
 
-public class CorrentistaVip {
+	public void setNomeResposavel(String nomeResposavel) {
+		this.nomeResposavel = nomeResposavel;
+	}
+
+	public static int getProxCodigo() {
+		return proxCodigo++;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
+
+
 
 }
