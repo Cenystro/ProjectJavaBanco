96e5506ffbcbdf634c83778e329cdf68ec6d1f1e
Cenystro <Cenystro@192.168.15.3>
2018-06-28 21:16:12 -0300

    Fiquei confuso quanto a aplicação de herança, retorno amanha para analisar melhor.

---
+++ SistemaBancario/src/CorrentistaJuridico.java
@@ -0,0 +1,38 @@
+
+public class CorrentistaJuridico {
+	private static int proxCodigo = 1;
+	private int codigo;
+	private String nome;
+	private String nomeResposavel;
+	
+	public CorrentistaJuridico (String nome, String nomeResponsavel){
+		this.codigo = getProxCodigo();
+		this.nome = nome;
+		
+	}
+	
+	public String getNomeResposavel() {
+		return nomeResposavel;
+	}
+
+	public void setNomeResposavel(String nomeResposavel) {
+		this.nomeResposavel = nomeResposavel;
+	}
+
+	public static int getProxCodigo() {
+		return proxCodigo++;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
